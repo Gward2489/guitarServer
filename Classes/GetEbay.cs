583c2416e607663c0b0f16d82ad1c7ef3e86f009
Gward2489 <gww8804@gmail.com>
2018-02-28 00:41:24 -0600

    comments r cool

--- Classes/GetEbay.cs
+++ Classes/GetEbay.cs
@@ -13,23 +13,34 @@ namespace guitarServer.Classes
     {
         public static async Task<string> GetGuitarsAsync(string path)
         {
+            // empty string to hold json string
             string JsonObj = "";
+            // create a new client
             using (HttpClient client = new HttpClient())
+            // get response from client with desire path 
             using (HttpResponseMessage res = await client.GetAsync(path))
+            // store content into new httpContent class
             using (HttpContent content = res.Content)
             {
+                // capture xml as string because using read as stream breaks app
                 string data = await content.ReadAsStringAsync();
+                // create empty xml document
                 XmlDocument doc = new XmlDocument();
+                // load xml doc with xml string
                 doc.LoadXml(data);
+                // turn it into a xmlNode
                 XmlNode newNode = doc.DocumentElement;
+                // turn it into a JSON string
                 JsonObj = JsonConvert.SerializeXmlNode(newNode);
 
+                //if we got something write it to the console
                 if (data != null)
                 {
                 Console.WriteLine(JsonObj);
                 }
                 
             }
+            //return Json object
             return JsonObj;
         }
     }
