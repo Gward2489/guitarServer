61568e36c6aeb870531a6d255ef8a12783b78379
Gward2489 <gww8804@gmail.com>
2018-03-17 18:15:59 -0500

    ready for testing with guitarPricer app basic search

--- Controllers/GuitarsController.cs
+++ Controllers/GuitarsController.cs
@@ -26,25 +26,13 @@ namespace guitarServer.Controllers
             _appSettings = appSettings;
         }
 
-        [HttpGet("{searchParams}")]
-        public async Task<IActionResult> GetAsync(string searchParams)
+
+        [HttpGet("{keyWords}")]
+        public async Task<IActionResult> GetBasicAsync(string keyWords)
         {
-            
-        string searchString = _appSettings.EbayKey;
-        Console.WriteLine(_appSettings);
-        Console.WriteLine(_appSettings.EbayKey);
-
-        searchString += searchParams;            
-        string results = await GetEbay.GetGuitarsAsync(searchString);
-        Console.WriteLine(results);
-        return Ok (results);
-        }
-
-        [HttpGet("{urlFirstHalf}/{urlSecondHalf}")]
-        public async Task<IActionResult> GetResultsAsync(string urlFirstHalf, string urlSecondHalf)
-        {
-
-            string searchUrl = urlFirstHalf + _appSettings.EbayKey + urlSecondHalf;
+            string urlFirstHalf = "http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findCompletedItems&SERVICE-VERSION=1.7.0&SECURITY-APPNAME=";
+            string urlSecondHalf = "&RESPONSE-DATA-FORMAT=XML&categoryId(0)=33034&categoryId(1)=33021&categoryId(2)=4713&itemFilter(0).name=SoldItemsOnly&itemFilter(0).value(0)=true&itemFilter(1).name=Condition&itemFilter(1).value(0)=Used&itemFilter(1).value(1)=2500&itemFilter(1).value(2)=3000&itemFilter(1).value(3)=4000&itemFilter(1).value(4)=5000&itemFilter(1).value(5)=6000&itemFilter(2).name=ExcludeCategory&itemFilter(2).value(0)=181223&itemFilter(2).value(1)=47067&REST-PAYLOAD&keywords=";
+            string searchUrl = urlFirstHalf + _appSettings.EbayKey + urlSecondHalf + keyWords;
             string results = await GetEbay.GetGuitarsAsync(searchUrl);
             Console.WriteLine(results);
             return Ok (results);
